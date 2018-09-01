d3ba92fd57658cad75ea182888068cd16fd4eea7
AlexTicovschi <alexmticovschi@gmail.com>
2018-09-01 11:13:39 +0100

    instantiating EventBrite and UI classes

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Instantiate both classes
+const eventBrite = new EventBrite();
+const ui = new UI();
+
+console.log(eventBrite);
---
+++ js/eventbrite.js
@@ -0,0 +1,5 @@
+class EventBrite {
+    constructor() {
+        this.token_auth = 'INA36KYEG6RHF3CFIB53'
+    }
+}
