37403cc6a11290c5bc67be67669a90743be5b127
AlexTicovschi <alexmticovschi@gmail.com>
2018-09-01 11:56:57 +0100

    querying the REST API and returning the categories

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -1,5 +1,17 @@
 class EventBrite {
     constructor() {
         this.token_auth = 'INA36KYEG6RHF3CFIB53'
+    } 
+
+    // Get categories from the API
+    async getCategoriesAPI() {
+        // Make a GET request and wait for the request to finish
+        const categoriesResponse = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${this.token_auth}`);
+    
+        // get the JSON version of the response
+        const categories = await categoriesResponse.json();
+
+        // return the object
+        return { categories }
     }
 }
--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,19 @@
 class UI {
     constructor() {
-        
+        // App initialization
+        this.init();
+    }
+    // Methods to be executed when the app is initialized
+    init() {
+
+        // Display categories in <select>
+        this.displayCategories();
+
+    }
+
+    // Display the categories
+    displayCategories() {
+        const categoriesList = eventBrite.getCategoriesAPI()
+            .then(data => console.log(data.categories));
     }
 }
